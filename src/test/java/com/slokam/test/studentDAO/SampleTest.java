ffd16d31b3216fae88c5c3bdd2d1e9066fc88875
Bharath <bharath.kristipati@gmail.com>
2017-06-22 09:11:32 +0530

    Added new testcase...

---
+++ src/test/java/com/slokam/test/studentDAO/SampleTest.java
@@ -0,0 +1,12 @@
+package com.slokam.test.studentDAO;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SampleTest {
+
+	@Test
+	public void test1(){
+		Assert.fail("Failing the test for fun...");
+	}
+}
